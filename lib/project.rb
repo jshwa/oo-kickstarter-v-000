1ea40d54f86fb0e84a68e35bf0c11e0505e5deb8
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-08 03:26:50 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,6 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    backer.backed_projects << self unless backer.backed_projects.includes?(self)
+    backer.backed_projects << self unless backer.backed_projects.include?(self)
   end
 end
